ea6c4455b126d3355cb1f7f0c02dcc003426f9d2
bemadeveloper <akbermet1990@icloud.com>
2024-01-30 19:53:07 +0600

    Add Extensions

--- loginScreenApp/Extensions/UIView+Extensions.swift
+++ loginScreenApp/Extensions/UIView+Extensions.swift
@@ -5,3 +5,23 @@ extension UIView {
         views.forEach { self.addSubview($0) }
     }
 }
+
+extension UITextField {
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+    
+    func setRightIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 10, y: 15, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        rightView = iconContainerView
+        rightViewMode = .always
+    }
+}
---
+++ loginScreenApp/Sources/SecondViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondViewController.swift
+//  loginScreenApp
+//
+//  Created by Bema on 30/1/24.
+//
+
+import Foundation
