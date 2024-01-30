85fdd5e56e92fb32a0c53c86b5ef11677a266cc0
bemadeveloper <akbermet1990@icloud.com>
2024-01-30 19:08:07 +0600

    SettingUpAProject

---
+++ loginScreenApp/App/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  loginScreenApp
+//
+//  Created by Bema on 30/1/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
---
+++ loginScreenApp/Sources/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    // MARK: - UI
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+
+
+    
+    
+    // MARK: - Setups
+    
+    private func setupHierarchy() {
+        view.addSubViews([
+            //button,
+            //label
+        ])
+    }
+}
+
